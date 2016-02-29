8a18a7dd4b6e481159f996896f9fd9ccd8265b95
Stian Kraggerud <stian@flytit.no>
2016-02-29 10:17:47 +0100

    ios

---
+++ src/ios/ThumCreator.h
@@ -0,0 +1,18 @@
+//
+//  ThumCreator.h
+//  PathPlugin
+//
+//  Created by Mangai on 22/01/16.
+//
+//
+
+#import <Cordova/CDVPlugin.h>
+
+@interface ThumCreator : CDVPlugin
+{
+    
+}
+- (void) cordovaGetFileContents:(CDVInvokedUrlCommand *)command;
+
+@end
+
